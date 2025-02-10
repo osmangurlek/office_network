4b02a1df806754b1602cfd1535904503c5b11fea
Osman Gürlek <osmangurlek10@gmail.com>
2025-02-10 14:39:26 +0300

    added database_url

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,5 +6,8 @@ load_dotenv()
 class Config:
     USERNAME=os.getenv("USERNAME")
     PASSWORD=os.getenv("PASSWORD")
+    MODEM_IP=os.getenv("MODEM_IP")
+    DEVICES_URL=os.getenv("DEVICES_URL")
+    DATABASE_URL=os.getenv("DATABASE_URL")
     
 settings = Config()
